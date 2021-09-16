5ebd429be474b0f6fddc75fc7b323d3d2dd78180
sosat1101 <air911q@gmail.com>
2021-09-17 07:47:10 +0800

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\StaticPagesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[StaticPagesController::class, 'home']);
+
+Route::get('/help',[StaticPagesController::class, 'help']);
+
+Route::get('/about',[StaticPagesController::class, 'about']);
